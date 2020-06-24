61978d3718eb9ce16d2611a5ab0d6282b72de738
Mauricio <eu@massaaki.com>
2020-06-23 23:14:06 -0300

    project(BankAccount): POO sample

---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BankAccount.Entities;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(5621, "Some Name", 10000.00, 50000.00);
+
+            Console.WriteLine(account.Balance);
+          
+        }
+    }
+}
