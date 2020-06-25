ebb9b9b6b2db5a41fdf91dcdda8c443d7b5b99b4
Mauricio <eu@massaaki.com>
2020-06-24 23:34:50 -0300

    project(EmployeePayment): Added Polymorphism Sample

---
+++ EmployeePayment/EmployeePayment/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayment.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AditionalCharge { get; set; }
+
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double aditionalCharge) 
+            : base(name, hours, valuePerHour)
+        {
+            AditionalCharge = aditionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AditionalCharge;
+        }
+    }
+}
